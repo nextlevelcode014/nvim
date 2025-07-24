182783f2fd26fef0409a0390adcdd86b098f9931
Marcelo Augusto <nextlevelcode014@gmail.com>
2025-07-24 14:35:57 -0300

    Updating

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,4 +1,5 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "ts_ls" }
+local servers = { "html", "cssls", "clangd", "ts_ls", "python-lsp-server" }
+
 vim.lsp.enable(servers)
